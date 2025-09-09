db0298984638afedd1a303fe4778bc77ff8ddb6f
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2025-09-09 13:56:09 +0200

    nvim: disable fzf picker

--- dot_config/nvim/lua/jp/plugins/fzf.lua
+++ dot_config/nvim/lua/jp/plugins/fzf.lua
@@ -1,7 +1,7 @@
 return {
   {
     "ibhagwan/fzf-lua",
-    enabled = true,
+    enabled = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local fzf_lua = require "fzf-lua"
